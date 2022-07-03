bc542ddf6c6aa6dab2dbd8893e5102d2771a1f2c
Сергей Щуклин <wtorkanorka.shuclin@yandex.ru>
2022-07-03 20:08:07 +0500

    Вылетают ошибки

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header/Header'
+import "./App.css"
+import Hero from './components/Hero/Hero.js'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  )
+}
+
+export default App
---
+++ my-app/src/components/Button/Button.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './button.module.css'
+
+export default function Button() {
+  return (
+    <button className={styles['button']}>Contact Us</button>
+  )
+}
